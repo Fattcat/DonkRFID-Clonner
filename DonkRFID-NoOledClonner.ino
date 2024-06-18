0f0888a0db17f8542f9b02b3797996026beb5da0
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-06-18 18:28:45 +0200

    Update and rename DonkRFID-Clonner-V2.ino to DonkRFID-NoOledClonner.ino

---
+++ DonkRFID-NoOledClonner.ino
@@ -0,0 +1,36 @@
+// Zapojenie komponentov:
+
+// MFRC522 RFID:
+// SDA -> D10
+// SCK -> D13
+// MOSI -> D11
+// MISO -> D12
+// IRQ -> N/A
+// GND -> GND
+// RST -> D9
+// 3.3V -> 3.3V
+
+// OLED displej:
+// VCC -> 5V
+// GND -> GND
+// SDA -> A4
+// SCL -> A5
+
+// SD Card:
+// VCC -> 5V
+// GND -> GND
+// MISO -> D12
+// MOSI -> D11
+// SCK -> D13
+// CS -> D8
+
+// Tlačidlá:
+// Tlačidlo UP -> D2
+// Tlačidlo DOWN -> D3
+// Tlačidlo ENTER -> D4
+
+// LED diódy:
+// Zelená LED -> D5 (cez rezistor)
+// Červená LED -> D6 (cez rezistor)
+
+
